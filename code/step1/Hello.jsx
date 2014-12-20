87adfc15397592a39933dbc43d482b9a789357d8
John Haugeland <stonecypher@gmail.com>
2014-12-19 18:50:40 -0800

    Step 1 tut code

---
+++ code/step1/Hello.jsx
@@ -0,0 +1,9 @@
+/** @jsx React.DOM */
+
+var Hello = React.createClass({
+
+    render: function() {
+        return <div>Hello, World!</div>;
+    }
+
+});
---
+++ code/step1/app.jsx
@@ -0,0 +1,3 @@
+/** @jsx React.DOM */
+
+React.render( <Hello />, document.body );
