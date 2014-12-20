560e2a1c4901c042cf69964812624f5a49aab8c0
John Haugeland <stonecypher@gmail.com>
2014-12-19 23:54:30 -0800

    step 2 underway

---
+++ code/step2/Hello.jsx
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+var Hello = React.createClass({
+
+    getDefaultProps: function() {
+        return {
+            toWhom: "World"
+        }
+    },
+
+    render: function() {
+        return <div>Hello, {this.props.toWhom}!</div>;
+    }
+
+});
